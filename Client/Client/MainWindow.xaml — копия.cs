dabb610db9d91e784dc42c3bb045f0e1e2b192b8
rinojk <sereymrlight@gmail.com>
2015-09-16 13:35:31 +0300

    Repository created

---
+++ Client/Client/MainWindow.xaml — копия.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace FileSend_Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void PathButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = false;
+            if (openFileDialog.ShowDialog()==true)
+            {
+                 pathTextBox.Text = openFileDialog.FileName;
+            }
+        }
+
+        private void SendButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            int port = Int32.Parse(PortTextBox.Text);
+            SendTCP(IPTextBox.Text, port, pathTextBox.Text);
+        }
+
+        private void Send(string IP, int port, string path)
+        {
+            byte[] buffer;
+            TcpClient client = new TcpClient(IP, port);
+            NetworkStream nStream = client.GetStream();
+            FileStream Fs = new FileStream(path, FileMode.Open, FileAccess.Read);
+            int NoOfPackets = Convert.ToInt32
+         (Math.Ceiling(Convert.ToDouble(Fs.Length) / Convert.ToDouble(1024)));
+            int TotalLength = (int)Fs.Length, CurrentPacketLength, counter = 0;
+            for (int i = 0; i < NoOfPackets; i++)
+            {
+                if (TotalLength > 1024)
+                {
+                    CurrentPacketLength = 1024;
+                    TotalLength = TotalLength - CurrentPacketLength;
+                }
+                else
+                {
+                    CurrentPacketLength = TotalLength;
+                    buffer = new byte[CurrentPacketLength];
+                    
+                    Fs.Read(buffer, 0, CurrentPacketLength);
+                    nStream.Write(buffer, 0, (int)buffer.Length);
+
+                }
+                Fs.Close();
+            }
+            nStream.Close();
+        }
+
+        private void SendTCP(string IPA, Int32 PortN, string M)
+        {
+            int BufferSize = 1024;
+            byte[] SendingBuffer = null;
+            TcpClient client = null;
+            NetworkStream netstream = null;
+            try
+            {
+                client = new TcpClient(IPA, PortN);
+                netstream = client.GetStream();
+                FileStream Fs = new FileStream(M, FileMode.Open, FileAccess.Read);
+                int NoOfPackets = Convert.ToInt32(Math.Ceiling(Convert.ToDouble(Fs.Length)/Convert.ToDouble(BufferSize)));
+                int TotalLength = (int) Fs.Length, CurrentPacketLength, counter = 0;
+                Stopwatch s = new Stopwatch();
+                s.Start();
+                for (int i = 0; i < NoOfPackets; i++)
+                {
+                    if (TotalLength > BufferSize)
+                    {
+                        CurrentPacketLength = BufferSize;
+                        TotalLength = TotalLength - CurrentPacketLength;
+                    }
+                    else
+                        CurrentPacketLength = TotalLength;
+                    SendingBuffer = new byte[CurrentPacketLength];
+                    Fs.Read(SendingBuffer, 0, CurrentPacketLength);
+                    netstream.Write(SendingBuffer, 0, (int) SendingBuffer.Length);
+                }
+                s.Stop();
+
+                MessageBox.Show(s.ElapsedMilliseconds.ToString());
+                Fs.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                netstream.Close();
+                client.Close();
+            }
+        }
+    }
+}
---
+++ FilesSend_Server/Server/MainWindow.xaml — копия.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FilesSend_Server
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private TcpClient client;
+
+        private void Receive(int port)
+        {
+            IPAddress ip = IPAddress.Any;
+            MessageBox.Show(ip.ToString());
+            TcpListener listener = new TcpListener(ip, port);
+            listener.Start();
+
+            byte[] data = new byte[1024];
+            int recBytes;
+
+            while (true)
+            {
+                if (listener.Pending())
+                {
+                    client = listener.AcceptTcpClient();
+                    NetworkStream nStream = client.GetStream();
+
+
+
+                    Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+                    Nullable<bool> result = dlg.ShowDialog();
+                    string filename = String.Empty;
+                    if (result == true)
+                    {
+                        filename = dlg.FileName;
+                    }
+
+
+                    if (!string.IsNullOrEmpty(filename))
+                    {
+                        int totalrecbytes = 0;
+                        FileStream Fs = new FileStream
+         (filename, FileMode.OpenOrCreate, FileAccess.Write);
+                        while ((recBytes = nStream.Read
+             (data, 0, data.Length)) > 0)
+                        {
+                            Fs.Write(data, 0, recBytes);
+                            totalrecbytes += recBytes;
+                        }
+                        Fs.Close();
+                    }
+
+                    nStream.Close();
+                    client.Close();
+                }
+            }
+        }
+
+        private void StartButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            int port = int.Parse(portTextBox.Text);
+            Task.Run(() => {Receive(port);});
+        }
+
+        private void PathButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private byte[] GetChecksum(string filename)
+        {
+            using (var md5 = MD5.Create())
+            {
+                using (var stream = File.OpenRead(filename))
+                {
+                    return md5.ComputeHash(stream);
+                }
+            }
+        }
+    }
+}
